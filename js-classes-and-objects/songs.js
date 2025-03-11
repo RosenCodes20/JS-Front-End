b82c2e8cdebf2c910182727c2bccacf54ba96dbb
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2025-03-11 17:40:19 +0200

    fix: fixed the song ex mistake.

--- js-classes-and-objects/songs.js
+++ js-classes-and-objects/songs.js
@@ -1,13 +1,43 @@
 function songs(arrOfStrings) {
+    let songs = [];
     class Song {
         typeList;
         name;
         time;
-
+        
         constructor(typeList, name, time) {
             this.typeList = typeList;
             this.name = name;
             this.time = time;
         }
     }
+    
+    let timesToIterate = arrOfStrings.shift();
+    let typeList = arrOfStrings.pop();
+    
+    for (let string of arrOfStrings) {
+        let [typeList, name, time] = string.split('_');
+        
+        let song = new Song(typeList, name, time);
+        
+        songs.push(song);
+    }
+    
+    for (let song of songs) {
+        if (typeList !== 'all') {
+            if (song.typeList == typeList) {
+                console.log(song.name);
+            }
+        } else {
+            console.log(song.name);
+        }
+    }
 }
+
+songs([2,
+
+    'like_Replay_3:15',
+    
+    'ban_Photoshop_3:48',
+    
+    'all'])
