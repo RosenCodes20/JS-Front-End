06025a7846ea907122b66f200219e6f29d9042e6
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2025-03-25 18:42:14 +0200

    feat: added the last exercise.

--- js-dom-introduction-exercise/09. Number Convertor/number-convertor.js
+++ js-dom-introduction-exercise/09. Number Convertor/number-convertor.js
@@ -1,3 +1,20 @@
 function solve() {
-    // TODO
+    let number = parseInt(document.getElementById('input').value, 10);
+
+    let selectedValue = document.getElementById('selectMenuTo').value;
+    let result;
+
+    if (!number || !selectedValue || isNaN(number)) {
+        return;
+    }
+
+    if (selectedValue == 'binary') {
+        result = number.toString(2);
+    } else if (selectedValue == 'hexadecimal') {
+        result = number.toString(16).toUpperCase();
+    }
+
+    let output = document.querySelector("[name='output']");
+
+    output.value = result;
 }
