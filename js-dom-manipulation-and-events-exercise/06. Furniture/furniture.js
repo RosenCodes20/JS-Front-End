669f8c82afdd883f934f9aeb71237dcc3c78c9ff
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2025-03-28 12:23:16 +0200

    feat: added more solutions.

--- js-dom-manipulation-and-events-exercise/06. Furniture/furniture.js
+++ js-dom-manipulation-and-events-exercise/06. Furniture/furniture.js
@@ -2,10 +2,93 @@ document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
     let generateBtn = document.querySelector("[value='Generate']");
+    let objectInput = document.querySelector('#input > textarea');
+    let buyBtn = document.querySelector("[value='Buy']");
 
     generateBtn.addEventListener('click', handleGenerateBtn);
+    buyBtn.addEventListener('click', handleBuyButton);
 
     function handleGenerateBtn(event) {
       event.preventDefault();
+      let table = document.querySelector('table tbody');
+      let parsedObj = JSON.parse(objectInput.value);
+
+      for (let obj of parsedObj) {
+        let newRow = document.createElement('tr');
+
+        let newImgData = document.createElement('td');
+        let newImg = document.createElement('img');
+        newImg.src = obj.img;
+        newImgData.appendChild(newImg);
+        newRow.appendChild(newImgData);
+
+        let newName = document.createElement('p');
+        let newNameData = document.createElement('td');
+        newName.textContent = obj['name'];
+
+        newNameData.appendChild(newName);
+        newRow.appendChild(newNameData);
+
+        let newPrice = document.createElement('p');
+        let newPriceData = document.createElement('td');
+        newPrice.textContent = obj['price'];
+
+        newPriceData.appendChild(newPrice);
+        newRow.appendChild(newPriceData);
+
+        let newDecFactor = document.createElement('p');
+        let newDecFactorData = document.createElement('td');
+
+        newDecFactor.textContent = obj.decFactor;
+
+        newDecFactorData.appendChild(newDecFactor);
+
+        newRow.appendChild(newDecFactorData);
+
+        let checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        let checkData = document.createElement('td');
+
+        checkData.appendChild(checkbox);
+
+        newRow.appendChild(checkData)
+        table.appendChild(newRow);
+      }
+      
+      objectInput.value = '';
+    }
+
+    function handleBuyButton(event) {
+      event.preventDefault();
+      let checkboxes = document.querySelectorAll('td > input');
+      let textarea = document.querySelector('#shop > textarea');
+      let boughtFurniture = [];
+      let prices = [];
+      let decFactorArr = [];
+
+      for (let checkbox of checkboxes) {
+        if (checkbox.checked) {
+            boughtFurniture.push(checkbox.parentElement.parentElement.children[1].children[0].textContent);
+            prices.push(Number(checkbox.parentElement.parentElement.children[2].children[0].textContent))
+            decFactorArr.push(Number(checkbox.parentElement.parentElement.children[3].children[0].textContent))
+          }
+      }
+      textarea.value += `Bought furniture: ${boughtFurniture.join(', ')}`;
+
+      let sum = 0;
+
+      for (let price of prices) {
+        sum += price;
+      }
+
+      textarea.value += `\nTotal price: ${sum}`;
+
+      let decFactorSum = 0;
+
+      for (let factor of decFactorArr) {
+        decFactorSum += factor;
+      }
+
+      textarea.value += `\nAverage decoration factor: ${decFactorSum / decFactorArr.length}`
     }
 }
--- js-dom-manipulation-and-events-exercise/07. Distance-Converter/distance-converter.js
+++ js-dom-manipulation-and-events-exercise/07. Distance-Converter/distance-converter.js
@@ -1,5 +1,64 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    let btn = document.getElementById('convert');
+
+    let inputDistance = document.getElementById('inputDistance');
+    let select = document.getElementById('inputUnits');
+    let outputUnit = document.getElementById('outputUnits');
+    btn.addEventListener('click', handleButtonClick);
+    let outputDistance = document.getElementById('outputDistance');
+
+    function handleButtonClick() {
+        let inputEnteredDistance = inputDistance.value;
+        let selectedInputUnit = select.value;
+        let selectedOutputUnit = outputUnit.value;
+        let toMeters = 0;
+        
+        let metersObj = {
+            'km': 0.001,
+            'cm': 100,
+            'mm': 1000,
+            'm': 1,
+            'mi': 0.0006213712,
+            'yrd': 1.0936132983,
+            'ft': 3.280839895,
+            'in': 39.3700787402,
+        }
+
+        function calculateMeters(toMeters, metersObj, selectedOutputUnit) {
+            toMeters *= metersObj[selectedOutputUnit];
+            return toMeters;
+        }
+
+        function returnMeters(outputDistance, toMeters, metersObj, selectedOutputUnit) {
+            outputDistance.value = calculateMeters(toMeters, metersObj, selectedOutputUnit);
+        }
+
+        if (selectedInputUnit == 'km') {
+            toMeters = inputEnteredDistance * 1000;
+            returnMeters(outputDistance, toMeters, metersObj, selectedOutputUnit)
+        } else if (selectedInputUnit == 'cm') {
+            toMeters = inputEnteredDistance * 0.01;
+            returnMeters(outputDistance, toMeters, metersObj, selectedOutputUnit)
+        } else if (selectedInputUnit == 'm') {
+            toMeters = inputEnteredDistance;
+            returnMeters(outputDistance, toMeters, metersObj, selectedOutputUnit)
+        } else if (selectedInputUnit == 'mm') {
+            toMeters = inputEnteredDistance * 0.001;
+            returnMeters(outputDistance, toMeters, metersObj, selectedOutputUnit)
+        } else if (selectedInputUnit == 'mi') {
+            toMeters = inputEnteredDistance * 1609.34;
+            returnMeters(outputDistance, toMeters, metersObj, selectedOutputUnit)
+        } else if (selectedInputUnit == 'yrd') {
+            toMeters = inputEnteredDistance * 0.9144;
+            returnMeters(outputDistance, toMeters, metersObj, selectedOutputUnit)
+        } else if (selectedInputUnit == 'ft') {
+            toMeters = inputEnteredDistance * 0.3048;
+            returnMeters(outputDistance, toMeters, metersObj, selectedOutputUnit)
+        } else if (selectedInputUnit == 'in') {
+            toMeters = inputEnteredDistance * 0.0254;
+            returnMeters(outputDistance, toMeters, metersObj, selectedOutputUnit)
+        }
+    }
 }
