bce2a8db19456965e507acde109bfa03db8e46c3
Rosen Ivanov <143969365+RosenCodes20@users.noreply.github.com>
2025-03-29 14:16:28 +0200

    feat: added the last ex - js quiz.

--- js-dom-manipulation-and-events-exercise/09. JavaScript-Quiz/quiz.js
+++ js-dom-manipulation-and-events-exercise/09. JavaScript-Quiz/quiz.js
@@ -1,5 +1,67 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-  //TODO
+    let sections = document.querySelectorAll('section');
+
+    let firstSect = sections[0];
+    let secondSect = sections[1];
+    let thirdSect = sections[2];
+    let rightAnswers = 0;
+    let answers = firstSect.getElementsByClassName('quiz-answer');
+    let results = document.getElementById('results');
+
+    let secondAnswers = secondSect.getElementsByClassName('quiz-answer');
+
+    let thirdAnswers = thirdSect.getElementsByClassName('quiz-answer');
+
+    for (let answer of answers) {
+      answer.addEventListener('click', handleFirstBtnClick);
+    }
+
+    for (let answer of secondAnswers) {
+      answer.addEventListener('click', handleSecondBtnClick);
+    }
+
+    for (let answer of thirdAnswers) {
+      answer.addEventListener('click', handleThirdBtnClick);
+    }
+
+    function handleFirstBtnClick(event) {
+      event.preventDefault();
+
+      if (event.target.textContent == 'onclick') {
+        rightAnswers++;
+      }
+
+      firstSect.classList.add('hidden');
+      secondSect.classList.remove('hidden');
+    }
+
+    function handleSecondBtnClick(event) {
+      event.preventDefault();
+
+      if (event.target.textContent == 'JSON.stringify()') {
+        rightAnswers++;
+      }
+
+      secondSect.classList.add('hidden');
+      thirdSect.classList.remove('hidden');
+    }
+
+    function handleThirdBtnClick(event) {
+      event.preventDefault();
+      if (event.target.textContent == 'A programming API for HTML and XML documents') {
+        rightAnswers++;
+      }
+
+      thirdSect.classList.add('hidden');
+
+      if (rightAnswers == 3) {
+        results.textContent = "You are recognized as top JavaScript fan!";
+      } else if (rightAnswers == 1) {
+        results.textContent = `You have ${rightAnswers} right answer`;
+      } else{
+        results.textContent = `You have ${rightAnswers} right answers`;
+      }
+    }
 }
